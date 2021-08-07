6d33e47fdce82cce493207e36bd5326fe988b3c7
Cole <coledebardelaben@gmail.com>
2021-08-06 21:08:27 -0500

    Initial Wyze Lock v2 Support

--- src/WyzeConnectedHome.js
+++ src/WyzeConnectedHome.js
@@ -5,6 +5,7 @@ const WyzeLight = require('./accessories/WyzeLight');
 const WyzeMeshLight = require('./accessories/WyzeMeshLight');
 const WyzeContactSensor = require('./accessories/WyzeContactSensor');
 const WyzeMotionSensor = require('./accessories/WyzeMotionSensor');
+const WyzeLock = require('./accessories/WyzeLock');
 
 const PLUGIN_NAME = 'homebridge-wyze-connected-home';
 const PLATFORM_NAME = 'WyzeConnectedHome';
@@ -127,6 +128,8 @@ module.exports = class WyzeConnectedHome {
         return WyzeContactSensor;
       case 'MotionSensor':
         return WyzeMotionSensor;
+      case 'Lock':
+        return WyzeLock;
     }
   }
 
