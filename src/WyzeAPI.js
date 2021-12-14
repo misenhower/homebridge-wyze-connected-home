1bcb57cedb5bc03dfe54ecc7bdb8e22d43fd5611
RMCob <49130485+RMCob@users.noreply.github.com>
2021-12-14 12:41:28 -0500

    Update WyzeAPI.js

--- src/WyzeAPI.js
+++ src/WyzeAPI.js
@@ -33,7 +33,7 @@ module.exports = class WyzeAPI {
     this.access_token = '';
     this.refresh_token = '';
     
-    this.dumpData = true;
+    // this.dumpData = true;
   }
 
   getRequestData(data = {}) {
@@ -94,10 +94,10 @@ module.exports = class WyzeAPI {
     try {
       result = await axios(config);
       this.log.debug(`API response: ${JSON.stringify(result.data)}`);
-      if (this.dumpData) {
-          this.log.info(`API response: ${JSON.stringify(result.data)}`);
-          this.dumpData = false;
-      }
+      // if (this.dumpData) {
+      //    this.log.info(`API response: ${JSON.stringify(result.data)}`);
+      //    this.dumpData = false;
+      // }
     } catch (e) {
       this.log.error(`Request failed: ${e}`);
 
